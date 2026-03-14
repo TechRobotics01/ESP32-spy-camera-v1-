480646236458f43b0183ef37c1ea95180ad7d0cc
Parth Dhiman <parthdhiman.pd@gmail.com>
2026-03-14 20:06:27 +0530

    added firmware and project files

--- firmware/ESP32_SPY_CAMERA/ESP32_SPY_CAMERA.ino
+++ firmware/ESP32_SPY_CAMERA/ESP32_SPY_CAMERA.ino
@@ -2,12 +2,11 @@ void setup() {
   // this is the test code for my esp32 camera module!!
   Serial.begin(9600);
   Serial.print("HELLO my esp32 spcamera ")
-  Serial.println("wakaapikey-"waka_76ed70b3-0541-4720-9c63-66e5e0eb5ec0"")
-  
+  Serial.println("wakaapikeyso")
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly: 
 
 }
