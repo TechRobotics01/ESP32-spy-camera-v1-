c1df8b40659b64c2f1129fb555c48d6e16932038
Parth Dhiman <parthdhiman.pd@gmail.com>
2026-03-14 20:08:56 +0530

    Update ESP32_SPY_CAMERA.ino

--- firmware/ESP32_SPY_CAMERA/ESP32_SPY_CAMERA.ino
+++ firmware/ESP32_SPY_CAMERA/ESP32_SPY_CAMERA.ino
@@ -2,7 +2,7 @@ void setup() {
   // this is the test code for my esp32 camera module!!
   Serial.begin(9600);
   Serial.print("HELLO my esp32 spcamera ")
-  Serial.println("wakaapikeyso")
+  Serial.println("wakaapikeyso") 
 
 }
 
